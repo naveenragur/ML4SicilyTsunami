beb359c15c50bda3e1db676ca786ff494d3a2c96
Naveen <naveen.raguramalingam@gmail.com>
2025-10-17 16:40:42 +0200

    Delete scripts/PaperIIPlots directory

--- scripts/PaperIIPlots/PaperI/r2/copyover_from_pseudoPoE.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ~/PAPERS/INUNDATION_WORK/pseudo_PoE_maps/flowdepth/percent_0010_Conly.pdf .
-cp ~/PAPERS/INUNDATION_WORK/pseudo_PoE_maps/flowdepth/NumberScenariosMap.pdf .
--- scripts/PaperIIPlots/PaperI/r2/make_2rfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python grid_and_r2fileread.py    \
-                   --gridfile     C_CT.grd    \
-                   --r2file       r_squared_train.nc   \
-                   --outfile      r2_topo_train.nc
-python grid_and_r2fileread.py    \
-                   --gridfile     C_CT.grd    \
-                   --r2file       r_squared_test.nc   \
-                   --outfile      r2_topo_test.nc
